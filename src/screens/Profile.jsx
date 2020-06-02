38174dc8428d8e1d0e790ddc47c307087777a749
Frunza <frunza.andrei.daniel@gmail.com>
2020-06-02 15:36:51 +0300

    results

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -5,10 +5,12 @@ import Card from "../components/Card";
 import { fetchProfile, fetchLikes, like, dislike } from "../utils/request";
 import Loader from "../components/Loader";
 import GoBack from "../components/GoBack";
+import Match from "../components/Match";
 
 const Profile = (props) => {
   const [profile, setProfile] = useState(null);
   const [likes, setLikes] = useState(null);
+  const [match, setMatch] = useState(false);
 
   useEffect(() => {
     const onMount = async () => {
@@ -25,11 +27,27 @@ const Profile = (props) => {
 
   const removeLike = () => {
     // 5. Find like index and splice it from state
+    const newLikes = [...likes];
+    const index = newLikes.findIndex((like) => like.id === profile.id);
+    newLikes.splice(index, 1);
+    setLikes(newLikes);
   };
 
   const onButtonClick = async (direction) => {
     // 4. removeLike from state
     // 6. likeOrDislike
+    const res =
+      direction === "right"
+        ? await like(profile.id)
+        : await dislike(profile.id);
+
+    if (res.match) {
+      const index = likes.findIndex((like) => like.id === profile.id);
+      const match = { ...likes[index] };
+      setMatch(match);
+    }
+
+    removeLike();
   };
 
   const _renderButtons = () => {
@@ -47,17 +65,25 @@ const Profile = (props) => {
 
   const isLikeable = () => {
     // 3. likes.some
+    return likes.some((like) => like.id === profile.id);
+  };
+
+  const onContinueSwiping = () => {
+    setMatch(null);
   };
 
   if (!profile) return <Loader />;
 
   return (
     <>
+      {match && (
+        <Match match={match} onContinueSwiping={onContinueSwiping}></Match>
+      )}
       <div className={styles.profile}>
         <GoBack />
         <div className={styles.hero}>
           <Card imgUrl={profile.profile_image}></Card>
-          {/* 2. If likeable render buttons */}
+          {isLikeable() && _renderButtons()}
         </div>
         <div className={styles.rightSide}>
           <h3 className={styles.name}>{profile.name}</h3>
